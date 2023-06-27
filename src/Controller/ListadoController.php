1542426fffd439129eec6010e0895cc17514ede7
Crisbr10 <borgescristhian1@gmail.com>
2023-06-27 19:30:39 +0200

    Completando algunas configuraciones

--- src/Controller/ListadoController.php
+++ src/Controller/ListadoController.php
@@ -7,9 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\DBAL\Connection;
 
-class ConnectDBController extends AbstractController
+class ListadoController extends AbstractController
 {
-    #[Route('/connect/d/b', name: 'listado_de_proveedores')]
+    #[Route('/listado_proveedores', name: 'listado_de_proveedores')]
     public function index(Connection $connection): Response
     {
         $sql = 'SELECT P.proveedor_id,P.nombre, P.correo_electronico, P.telefono_contacto, TP.nombre AS tipo_nombre, P.activo, DATE(P.fecha_creacion) AS fecha_creacion, DATE(P.fecha_actualizacion) AS fecha_actualizacion
@@ -17,7 +17,7 @@ class ConnectDBController extends AbstractController
                 JOIN TiposProveedores TP ON P.tipo_id = TP.tipo_id';
         $proveedores = $connection->fetchAllAssociative($sql);
 
-        return $this->render('connect_db/index.html.twig', [
+        return $this->render('listado_proveedores/index.html.twig', [
         'proveedores' => $proveedores,
         ]);
     }
