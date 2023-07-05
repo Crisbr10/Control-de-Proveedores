6e2f25caafc9f25b8b5e809f393c34f417036ab9
Crisbr10 <borgescristhian1@gmail.com>
2023-07-05 14:10:42 +0200

    Implementacion de doctrine completada

--- src/Controller/EliminarProveedorController.php
+++ src/Controller/EliminarProveedorController.php
@@ -5,18 +5,35 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\DBAL\Connection;
+use Symfony\Component\HttpFoundation\Request;
+USE Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Proveedores;
 
-class EliminarProveedorController extends AbstractController
-{
+
+class EliminarProveedorController extends AbstractController{
+
+    private $em;
+    /**
+     * Constructor de la clase.
+     * 
+     * @param EntityManagerInterface $em El administrador de entidades de Doctrine
+     */
+   
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em=$em;
+    }
+
     #[Route('/proveedores/eliminar/{id}', name: 'eliminar_proveedor')]
-    public function eliminar(Connection $connection, $id): Response
+    public function eliminar(Request $request, $id): Response
     {
         // Realizar la eliminación en la base de datos
-        $sql = 'DELETE FROM Proveedores WHERE proveedor_id = ?';
-        $connection->executeQuery($sql, [$id]);
+        $proveedor = $this->em->getRepository(Proveedores::class)->find($id);
+
+        $this->em->remove($proveedor);
+        $this->em->flush();
 
         // Redireccionar a la página de listado de proveedores
-        return $this->redirectToRoute('listado_de_proveedores');
+        return $this->redirectToRoute('app_lista_proveedores');
     }
 }
--- src/Controller/InsertarProveedorController.php
+++ src/Controller/InsertarProveedorController.php
@@ -7,22 +7,42 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\DBAL\Connection;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Proveedores;
+use App\Entity\Tiposproveedores;
 
 class InsertarProveedorController extends AbstractController{
+
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+    
     #[Route('/insertar/proveedor', name: 'insertar_proveedor')]
     public function index(Request $request, Connection $connection): Response{
         if ($request->isMethod('POST')) {
             $nombre = $request->request->get('nombre');
             $correo = $request->request->get('correo');
             $telefono = $request->request->get('telefono');
-            $tipoId = $request->request->get('tipo_id');
-            $activo = $request->request->get('activo')=== '1'? 1 : 0;
+            $tipo = $request->request->get('tipo_id');
+            $tipo = $this->em->getRepository(Tiposproveedores::class)->find($tipo);
+            $activo = $request->request->get('activo')=== 1? 1 : 0;
 
-            // Realizar la inserción en la base de datos
-            $sql = 'INSERT INTO Proveedores (nombre, correo_electronico, telefono_contacto, tipo_id, activo)
-                    VALUES (?, ?, ?, ?, ?)';
-            $parameters = [$nombre, $correo, $telefono, $tipoId, $activo];
-            $connection->executeQuery($sql, $parameters);
+            // Crear una nueva instancia de Proveedores y asignar los valores
+            $proveedor = new Proveedores();
+            $proveedor->setNombre($nombre);
+            $proveedor->setCorreoElectronico($correo);
+            $proveedor->setTelefonoContacto($telefono);
+            $proveedor->setTipo($tipo);
+            $proveedor->setActivo($activo);
+            $proveedor->setFechaCreacion(new \DateTime());
+            $proveedor->setFechaActualizacion(new \DateTime());
+
+            // Persistir y guardar el proveedor en la base de datos
+            $this->em->persist($proveedor);
+            $this->em->flush();
 
             // Redireccionar a la página de listado de proveedores
             return $this->redirectToRoute('app_lista_proveedores');
